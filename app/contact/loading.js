1a8f6978567ea3da885246225b2220bd287fd305
vojmax <vojislav.maksovic@gmail.com>
2023-05-25 13:14:22 +0200

    added loading spinner animation to the loading files

--- app/contact/loading.js
+++ app/contact/loading.js
@@ -1,16 +1,14 @@
 export default function Loading() {
 	return (
-		<div className="bg-light flex h-screen w-screen text-center items-center justify-center text-dark dark:text-light">
-			Loading...
-		</div>
+		<section className="flex w-screen h-screen align-middle items-center justify-center bg-light dark:bg-dark">
+			<div
+				class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-dark border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] dark:border-light"
+				role="status"
+			>
+				<span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+					Loading...
+				</span>
+			</div>
+		</section>
 	);
 }
-
-// <div
-// 	class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-// 	role="status"
-// >
-// 	<span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-// 		Loading...
-// 	</span>
-// </div>
